6dc59645ff242eeb921215b84c647e0307951960
Christoffer Arnlund <christoffer.arnlund@gmail.com>
2017-05-01 16:07:38 +0200

    Start

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ Created on Mon May  1 15:37:10 2017
 
 @author: Christoffer Arnlund & Oskar Friberg
 """
+
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+import numpy
+
+x=numpy.arange(0, 3.5, 0.5)
+y=[70,70,55,22,13,10,10]
+cs = CubicSpline(x, y,bc_type='clamped')
+plt.figure()
+plt.plot(x, y, 'o', label='data')
+plt.plot(x, cs(x), label="S")
+#plt.plot(x, cs(x, 1), label="S'")
+print(x)
