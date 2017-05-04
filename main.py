5212132a6180571fc043323ea5ccda9151b45603
Christoffer Arnlund <christoffer.arnlund@gmail.com>
2017-05-04 15:49:45 +0200

    Fixed errors

--- main.py
+++ main.py
@@ -7,35 +7,45 @@ Created on Mon May  1 15:37:10 2017
 
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import interp1d
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as op
 
-x=np.arange(0, 301, 50)
+x=np.arange(0, 3.5,0.5)
 y=[70,70,55,22,13,10,10]
 cs = CubicSpline(x, y,bc_type='clamped')
-
-xs = np.arange(-50, 450, 0.1)
+thermocline=op.fsolve(cs, 1, args=2)
+print("thermocline:",thermocline)
+xs = np.arange(0,4, 0.01)
 
 f = interp1d(x, y)
 #f2 = interp1d(x, y, kind='cubic')
 
+def fluxFunc(x):
+     
+        return -1*cs(x,1)
+
+flux=fluxFunc(thermocline)
+print("Task 2 flux: ",flux.item(0))
 plt.figure()
 plt.plot(x, y, 'o', label='data')
-plt.plot(xs, cs(xs), label="S'")
+plt.plot(xs, cs(xs), label="C (℃/m)")
+#plt.figure()
+plt.plot(xs, cs(xs, 1), label="C'")
+#plt.figure()
+plt.plot(xs, cs(xs, 2), label="C''")
+plt.legend()
 plt.figure()
-plt.plot(xs,-0.001*cs(xs,1), label="J")
-plt.figure()
-plt.plot(xs, cs(xs, 1), label="S'")
-plt.figure()
-plt.plot(xs, cs(xs, 2), label="S''")
-#plt.plot(xs, cs(xs, 3), label="S'''")
-#plt.ylim(-100, 100)
-
+plt.plot(xs,-1*cs(xs,1), label="J")
+plt.legend()
 
 #plt.plot(x, f(x), label="S'")
 plt.show()
-res= cs.solve(50)
-print(res)
-print(cs)
-print(x)
+
+tempat17=cs(1.7)
+depthat50= cs.solve(50)
+print("depthat50",depthat50)
+print("temp at 1.7m ",tempat17)
+#print(cs)
+#print(x)
