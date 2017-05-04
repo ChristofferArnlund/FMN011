1174cfb8502feda5be953545c3adb5d6216fb30d
Christoffer Arnlund <christoffer.arnlund@gmail.com>
2017-05-04 16:07:56 +0200

    Refactored

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ Created on Mon May  1 15:37:10 2017
 """
 
 from scipy.interpolate import CubicSpline
-from scipy.interpolate import interp1d
-
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as op
@@ -16,28 +14,29 @@ x=np.arange(0, 3.5,0.5)
 y=[70,70,55,22,13,10,10]
 cs = CubicSpline(x, y,bc_type='clamped')
 thermocline=op.fsolve(cs, 1, args=2)
-print("thermocline:",thermocline)
-xs = np.arange(0,4, 0.01)
 
-f = interp1d(x, y)
-#f2 = interp1d(x, y, kind='cubic')
+print("The thermocline depth is:",thermocline.item(0),"m.")
+
+xs = np.arange(0,4, 0.01)
 
 def fluxFunc(x):
      
         return -1*cs(x,1)
 
 flux=fluxFunc(thermocline)
-print("Task 2 flux: ",flux.item(0))
+print("The flux at",thermocline.item(0),"m is ",flux.item(0))
 plt.figure()
 plt.plot(x, y, 'o', label='data')
+
 plt.plot(xs, cs(xs), label="C (℃/m)")
-#plt.figure()
+
 plt.plot(xs, cs(xs, 1), label="C'")
-#plt.figure()
+
 plt.plot(xs, cs(xs, 2), label="C''")
+
 plt.legend()
 plt.figure()
-plt.plot(xs,-1*cs(xs,1), label="J")
+plt.plot(xs,-1*cs(xs,1), label=r"J $ \left( \frac{cal}{m^2s}\right)$")
 plt.legend()
 
 #plt.plot(x, f(x), label="S'")
@@ -45,7 +44,6 @@ plt.show()
 
 tempat17=cs(1.7)
 depthat50= cs.solve(50)
-print("depthat50",depthat50)
-print("temp at 1.7m ",tempat17)
-#print(cs)
-#print(x)
+
+print("Depth at 50 degrees is ",depthat50.item(0), "m")
+print("Temperature at 1.7 m is ",tempat17.item(0))
